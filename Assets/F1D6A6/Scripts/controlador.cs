9e78e407870fb80c32711d50050cf9e255092371
vincealejandroherrera151 <78968113+vincealejandroherrera151@users.noreply.github.com>
2022-10-06 12:52:50 -0500

    Avance de 06/10/2022 pruebas
    Se inicializaron las pruebas del minijuego

---
+++ Assets/F1D6A6/Scripts/controlador.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class controlador : MonoBehaviour
+{
+    public GameObject[] objects;
+    public int sigAPisar = 0;
+    public bool regreso;
+    // Start is called before the first frame update
+
+    public IEnumerator iniciarJuego()
+    {
+        yield return new WaitForSeconds(1f);
+        objects[0].GetComponent<objetosPisar>().subir();
+        yield return new WaitForSeconds(.5f);
+        objects[1].GetComponent<objetosPisar>().subir();
+        sigAPisar = 0;
+    }
+    void Start()
+    {
+        StartCoroutine(iniciarJuego());
+        Debug.Log("Se inicializo la corrutina: ");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (sigAPisar<objects.Length)
+        {
+            if (objects[sigAPisar].GetComponent<objetosPisar>().mePisaron)
+            {
+                if (sigAPisar + 2 < objects.Length)
+                {
+                    objects[sigAPisar + 2].GetComponent<objetosPisar>().subir();
+                }
+                if (sigAPisar - 2 >= 0)
+                {
+                    objects[sigAPisar - 2].GetComponent<objetosPisar>().bajar();
+                }
+                sigAPisar += 1;
+            }
+        }*/
+        if (regreso == false)
+        {
+            if (sigAPisar < objects.Length)
+            {
+                if (objects[sigAPisar].GetComponent<objetosPisar>().mePisaron)
+                {
+                    if (sigAPisar + 2 < objects.Length)
+                    {
+                        objects[sigAPisar + 2].GetComponent<objetosPisar>().subir();
+                    }
+                    if (sigAPisar - 2 >= 0)
+                    {
+                        objects[sigAPisar - 2].GetComponent<objetosPisar>().bajar();
+                        objects[sigAPisar - 2].GetComponent<objetosPisar>().mePisaron=false;
+                        //objects[sigAPisar - 1].GetComponent<objetosPisar>().mePisaron = false;
+                    }
+                    if (sigAPisar == objects.Length-1) {
+                        regreso = true;
+                        sigAPisar -= 1;
+                    }
+
+                    sigAPisar += 1;
+                }
+            }
+        }
+        else
+        {
+            if (regreso == true) {
+                if (sigAPisar >= 0)
+                {
+                    if (objects[sigAPisar].GetComponent<objetosPisar>().mePisaron)
+                    {
+                        if (sigAPisar - 2 >= 0)
+                        {
+                            objects[sigAPisar - 2].GetComponent<objetosPisar>().subir();
+                        }
+                        if (sigAPisar + 2 < objects.Length)
+                        {
+                            objects[sigAPisar + 2].GetComponent<objetosPisar>().bajar();
+                        }
+                        sigAPisar -= 1;
+                    }
+                } 
+            }
+        }
+    }
+}
---
+++ Assets/F1D6A6/Scripts/objetosPisar.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Valve.VR.InteractionSystem;
+
+
+namespace Valve.VR.InteractionSystem
+{
+
+    public class objetosPisar : MonoBehaviour
+    {
+        public Interactable inCube;
+        public bool mePisaron;
+        public Animator anim;
+        public bool ambosPies;
+        //public GameObject manoDer, manoIzq;
+
+        public void subir()
+        {
+            anim.SetTrigger("subir");
+        }
+
+        public void bajar()
+        {
+            anim.SetTrigger("bajar");
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+            inCube = GetComponent<Interactable>();
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.blue);
+            //controlador = GameObject.Find("objetosPisar").GetComponent<Controlador>();
+        }
+
+        private void Update()
+        {
+            if (inCube.isHovering)
+            {
+                Debug.Log(inCube.hoveringHand.handType);
+                if (ambosPies) {
+                    if (inCube.hoveringHands.ToArray().Length > 1)// == SteamVR_Input_Sources.LeftHand
+                    {
+                        mePisaron = true;
+                    }
+                }
+                else
+                {
+                    mePisaron = true;
+                }
+                
+                Debug.Log(transform.name);
+            }
+            else
+            {
+                mePisaron = false;
+            }
+        }
+        /*private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.name);
+        }*/
+    }
+}
